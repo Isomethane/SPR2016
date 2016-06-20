90afc3eec39b8bdf94fada277bac9ab83e5ce277
Isomethane <mn.malysheva@mail.ru>
2016-06-20 22:27:39 +0300

    More informative alerts.

--- final/js/animation/render/render.js
+++ final/js/animation/render/render.js
@@ -6,16 +6,16 @@ function render() {
             gl.viewportWidth = this.canvas.width;
             gl.viewportHeight = this.canvas.height;
         } catch (e) {
-            alert("Could not initialise WebGL 2.");
+            alert("Could not initialise WebGL 2. Enable WebGL 2.0 prototype in your browser settings.");
         }
         if (!gl) {
-            alert("Could not initialise WebGL 2.");
+            alert("Could not initialise WebGL 2. Enable WebGL 2.0 prototype in your browser settings.");
         }
 
         if (!gl.getExtension("OES_texture_float_linear"))
-            alert("Could not get 'OES_texture_float_linear' extension.");
+            alert("Could not get 'OES_texture_float_linear' extension. Enable WebGL draft extensions in your browser settings.");
         if (!gl.getExtension("WEBGL_color_buffer_float") && !gl.getExtension("EXT_color_buffer_float"))
-            alert("Could not get 'color_buffer_float' extension.");
+            alert("Could not get 'color_buffer_float' extension. Enable WebGL draft extensions in your browser settings.");
 
         gl.enable(gl.DEPTH_TEST);
         gl.clearColor(0.0, 0.0, 0.0, 1.0);
@@ -34,8 +34,6 @@ function render() {
             self.canvas.height = window.innerHeight - 100;
         }, false);
         this.initGL();
-        gl.enable(gl.DEPTH_TEST);
-        gl.clearColor(0.0, 0.0, 0.0, 1.0);
 
         this.container = document.createElement('div');
         document.body.appendChild(this.container);
